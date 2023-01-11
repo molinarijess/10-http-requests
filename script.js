c0dd2c8f7aff32807cc26b564abcca4e662ec806
molinarijess <lacerdajess@gmail.com>
2023-01-11 17:01:06 +0000

    lesson using axios

--- script.js
+++ script.js
@@ -4,54 +4,72 @@ const form = document.getElementById("new-post");
 const fetchButton = document.querySelector("#available-posts button");
 const postList = document.querySelector("ul");
 
-function sendHttppRequest(method, url, data) {
-  // const promise = new Promise((resolve, reject) => {
-  //   const xhr = new XMLHttpRequest();
-  //   xhr.open(method, url);
-  //   xhr.responseType = "json";
+//function sendHttppRequest(method, url, data) {
+// const promise = new Promise((resolve, reject) => {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open(method, url);
+//   xhr.responseType = "json";
 
-  //   xhr.onload = function () {
-  //     if (xhr.status >= 200 && xhr.status < 300) {
-  //       resolve(xhr.response);
-  //     } else {
-  //       reject(new Error("something went wrong!"));
-  //     }
-  //   };
+//   xhr.onload = function () {
+//     if (xhr.status >= 200 && xhr.status < 300) {
+//       resolve(xhr.response);
+//     } else {
+//       reject(new Error("something went wrong!"));
+//     }
+//   };
 
-  //   xhr.onerror = function () {
-  //     reject(new Error("failed to send request!"));
-  //   };
+//   xhr.onerror = function () {
+//     reject(new Error("failed to send request!"));
+//   };
 
-  //   xhr.send(JSON.stringify(data));
-  // });
+//   xhr.send(JSON.stringify(data));
+// });
 
-  // return promise;
+// return promise;
 
-  return fetch(url, {
-    method: method,
-    body: JSON.stringify(data),
-  }).then((response) => {
-    return response.json();
-  });
-}
+//   return fetch(url, {
+//     method: method,
+//     body: JSON.stringify(data),
+//   }).then((response) => {
+//     return response.json();
+//   });
+// }
 
-function fetchPosts() {
-  //try {
-  sendHttppRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
-    (responseData) => {
-      const listOfPosts = responseData;
-      for (const post of listOfPosts) {
-        const postEl = document.importNode(postTemplate.content, true);
-        postEl.querySelector("h2").textContent = post.title.toUpperCase();
-        postEl.querySelector("p").textContent = post.body;
-        postEl.querySelector("li").id = post.id;
-        listElement.append(postEl);
-      }
+//function fetchPosts() {
+//try {
+// sendHttppRequest("GET", "https://jsonplaceholder.typicode.com/posts").then(
+//   (responseData) => {
+//     const listOfPosts = responseData;
+//     for (const post of listOfPosts) {
+//       const postEl = document.importNode(postTemplate.content, true);
+//       postEl.querySelector("h2").textContent = post.title.toUpperCase();
+//       postEl.querySelector("p").textContent = post.body;
+//       postEl.querySelector("li").id = post.id;
+//       listElement.append(postEl);
+//     }
+//   }
+// );
+// } catch (error) {
+//   alert(error.message);
+// }
+// }
+
+async function fetchPosts() {
+  try {
+    const response = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+    const listOfPosts = response.data;
+    for (const post of listOfPosts) {
+      const postEl = document.importNode(postTemplate.content, true);
+      postEl.querySelector("h2").textContent = post.title.toUpperCase();
+      postEl.querySelector("p").textContent = post.body;
+      postEl.querySelector("li").id = post.id;
+      listElement.append(postEl);
     }
-  );
-  // } catch (error) {
-  //   alert(error.message);
-  // }
+  } catch (error) {
+    alert(error.message);
+  }
 }
 
 async function createPost(title, content) {
@@ -62,7 +80,11 @@ async function createPost(title, content) {
     userId: userId,
   };
 
-  sendHttppRequest("POST", "https:jsonplaceholder.typicode.com/posts", post);
+  const response = await axios.post(
+    "https:jsonplaceholder.typicode.com/posts",
+    post
+  );
+  console.log(response);
 }
 
 fetchButton.addEventListener("click", fetchPosts);
@@ -78,10 +100,7 @@ form.addEventListener("submit", (event) => {
 postList.addEventListener("click", (event) => {
   if (event.target.tagName === "BUTTON") {
     const postId = event.target.closest("li").id;
-    sendHttppRequest(
-      "DELETE",
-      `https://jsonplaceholder.typicode.com/posts/${postId}`
-    );
+    axios.delete(`https://jsonplaceholder.typicode.com/posts/${postId}`);
 
     document.getElementById(postId).remove();
   }
